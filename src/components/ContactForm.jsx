2e10e2fa640b1605121b272069399ca5386b92f1
Nadya <timrova.nadiia1622@vu.cdu.edu.ua>
2024-02-03 18:39:25 +0200

    6

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -6,29 +6,40 @@ import { addContact } from '../redux/actions';
 import styles from '../styles/ContactForm.module.css';
 
 const ContactForm = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    number: '',
-  });
+  // const [formData, setFormData] = useState({
+  //   name: '',
+  //   number: '',
+  // });
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
   const dispatch = useDispatch();
 
+  // const onSubmitForm = (event) => {
+  //   event.preventDefault();
+  //   dispatch(addContact(formData.name, formData.number));
+  //   resetForm();
+  // };
   const onSubmitForm = (event) => {
     event.preventDefault();
-    dispatch(addContact(formData.name, formData.number));
-    resetForm();
+    const form = event.currentTarget;
+    const { name, number } = form.elements;
+    dispatch(addContact(name.value, number.value));
+    form.reset();
   };
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  const handleChangeName = (e) => {
+    setName(e.target.value);
+  };
+  const handleChangeNum = (e) => {
+    setNumber(e.target.value);
   };
 
-  const resetForm = () => {
-    setFormData({ name: '', number: '' });
-  };
+  // const resetForm = () => {
+  //   setFormData({ name: '', number: '' });
+  // };
 
-  const { name, number } = formData;
+  // const { name, number } = formData;
 
   return (
     <form className={styles.contactForm} onSubmit={onSubmitForm}>
@@ -39,7 +50,7 @@ const ContactForm = () => {
           type="text"
           name="name"
           value={name}
-          onChange={handleChange}
+          onChange={handleChangeName}
           required
         />
       </label>
@@ -50,7 +61,7 @@ const ContactForm = () => {
           type="tel"
           name="number"
           value={number}
-          onChange={handleChange}
+          onChange={handleChangeNum}
           required
         />
       </label>
